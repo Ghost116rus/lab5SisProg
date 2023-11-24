578b9593da89f49a8de0fd883a0a253f3884cf57
Данил Калеев <KaleevDA@stud.kai.ru>
2023-11-24 11:55:37 +0300

    Поправил ошибку с переполнением

--- lab1SisProg/FSPass.cs
+++ lab1SisProg/FSPass.cs
@@ -298,6 +298,11 @@ namespace lab1SisProg
                                         return false;
                                     }
                                 }
+                                else
+                                {
+                                    errorText = $"В строке {i + 1} ошибка. Отрицательное число либо превышено максимальное значение числа";
+                                    return false;
+                                }
                             }
                             else
                             {
